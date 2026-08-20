d7b9259fc9b49dac01c7e74b452d2f25625ec55a
Fredrixz <fredrik.ekdahl87@gmail.com>
2026-08-20 09:57:28 +0200

    Steg 2: Lägg till /blockchain-endpoint med transaktionslista

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ app.get('/', (req, res) => {
   res.send('Hello från Express!');
 });
 
+app.get('/blockchain', (req, res) => {
+  const transactions = [
+    { sender: '', recipient: '', batchId: '1', weightKg: '' },
+    { sender: '', recipient: '', batchId: '2', weightKg: '' }
+  ];
+
+  return res.json(transactions);
+});
+
 module.exports = app;
