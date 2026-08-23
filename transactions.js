103b8c4f1da875771d2d92aa3e2d5d56e5bbb9c0
Fredrixz <fredrik.ekdahl87@gmail.com>
2026-08-23 20:05:39 +0200

    Städa bort transactions.js - ersatt av Blockchain.pendingTransactions

--- transactions.js
+++
@@ -1,9 +0,0 @@
-export const transactions = [
-  { sender: '', recipient: '', batchId: '1', weightKg: '' },
-  { sender: '', recipient: '', batchId: '2', weightKg: '' }
-];
-
-export function addTransaction(transaction) {
-  transactions.push(transaction);
-  return transaction;
-}
