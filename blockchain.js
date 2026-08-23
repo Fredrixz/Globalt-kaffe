8fd2a4c940627bc1f3525c36fec63f6b0649836d
Fredrixz <fredrik.ekdahl87@gmail.com>
2026-08-23 20:04:28 +0200

    Grön: implementera isChainValid

--- blockchain.js
+++ blockchain.js
@@ -38,4 +38,21 @@ export class Blockchain {
 
     return newBlock;
   }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
